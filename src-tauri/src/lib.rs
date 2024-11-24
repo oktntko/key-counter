0c0f21a5ee5702a96f6f7cff0e3949839eedc464
oktntko <oktntko@gmail.com>
2024-11-24 23:10:45 +0900

    rdev

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,62 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use log::info;
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    info!("The name param is {}", name);
-
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::thread;
+use rdev::{listen, Event, EventType};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::LogDir {
+                        file_name: Some("logs".to_string()),
+                    },
+                ))
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(move |_| {
+            let callback = move |event: Event| {
+                match event.name {
+                    Some(string) => {
+                        info!("rdev callback Some: {}", string);
+                    }
+                    None => {
+                        match event.event_type {
+                            EventType::KeyPress(key) => {
+                                let key_str = format!("{:?}", key);
+                                info!("rdev callback KeyPress: {} {:?}", key_str, key);
+                            }
+                            EventType::KeyRelease(key) => {
+                                let key_str = format!("{:?}", key);
+                                info!("rdev callback KeyRelease: {} {:?}", key_str, key);
+                            }
+                            EventType::ButtonPress { .. } => {
+                                // Ignore ButtonPress event type マウスボタン
+                            }
+                            EventType::ButtonRelease { .. } => {
+                                // Ignore ButtonRelease event type マウスボタン
+                            }
+                            EventType::MouseMove { .. } => {
+                                // Ignore MouseMove event type
+                            }
+                            EventType::Wheel { .. } => {
+                                // Ignore Wheel event type
+                            }
+                        }
+                    }
+                }
+            };
+
+            thread::spawn(move || {
+                if let Err(error) = listen(callback) {
+                    info!("Error: {:?}", error);
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,51 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod keyboard_listener;
-use std::thread;
-use tauri::Manager;
-use tauri::Emitter;
-use log::info;
-
-#[derive(Clone, serde::Serialize)]
-struct Payload {
-    mode: String,
-    message: String,
-}
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    info!("The name param is {}", name);
-
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .setup(move |app| {
-            let wv = app.get_webview_window("main").unwrap();
-
-            thread::spawn(move || {
-                keyboard_listener::run_listener(move |s: &str, s1: &str| {
-                    info!("The name param is {} {}", s, s1);
-                    if let Err(err) = wv.emit(
-                        "keypress",
-                        Payload {
-                            mode: String::from(s),
-                            message: String::from(s1),
-                        },
-                    ) {
-                        eprintln!("Error while emitting event: {:?}", err);
-                    }
-                })
-            });
-
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    key_counter_lib::run()
 }
